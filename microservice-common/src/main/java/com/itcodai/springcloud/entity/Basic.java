756cebee6bf4fbe90bcc55b42515e6d01da8d6f2
mazhenqing <1265044234@qq.com>
2019-08-14 21:35:35 +0800

    改了

--- microservice-common/src/main/java/com/itcodai/springcloud/entity/Basic.java
+++ microservice-common/src/main/java/com/itcodai/springcloud/entity/Basic.java
@@ -1,4 +1,49 @@
 package com.itcodai.springcloud.entity;
 
 public class Basic {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWheel_plantingone() {
+        return wheel_plantingone;
+    }
+
+    public void setWheel_plantingone(String wheel_plantingone) {
+        this.wheel_plantingone = wheel_plantingone;
+    }
+
+    public String getWheel_plantingtwo() {
+        return wheel_plantingtwo;
+    }
+
+    public void setWheel_plantingtwo(String wheel_plantingtwo) {
+        this.wheel_plantingtwo = wheel_plantingtwo;
+    }
+
+    public String getWheel_plantingthree() {
+        return wheel_plantingthree;
+    }
+
+    public void setWheel_plantingthree(String wheel_plantingthree) {
+        this.wheel_plantingthree = wheel_plantingthree;
+    }
+
+    public String getWheel_plantingfour() {
+        return wheel_plantingfour;
+    }
+
+    public void setWheel_plantingfour(String wheel_plantingfour) {
+        this.wheel_plantingfour = wheel_plantingfour;
+    }
+
+    private int id;
+    private String wheel_plantingone;
+    private String wheel_plantingtwo;
+    private String wheel_plantingthree;
+    private String wheel_plantingfour;
 }
