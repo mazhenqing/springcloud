d61814ae9720fa408652b3dd1fe69c573aded96c
mazhenqing <1265044234@qq.com>
2019-09-24 11:46:58 +0800

    9月24号修改

--- microservice-config-client/src/main/java/springcloud/controller/ClientController.java
+++ microservice-config-client/src/main/java/springcloud/controller/ClientController.java
@@ -1,21 +1,20 @@
 package springcloud.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RestController
 public class ClientController {
-//    @Value("${from}")
-//    private String from;
-    @Autowired
-    private Environment env;
+    @Value("${from}")
+    private String from;
+//    @Autowired
+//    private Environment env;
     @RequestMapping("/from")
     public String from(){
-//        return this.from;
-        return env.getProperty("from");
+        return this.from;
+//        return env.getProperty("from");
     }
 }
